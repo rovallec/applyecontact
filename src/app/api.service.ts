59014f448b0beb708aec8dcc3431c3ac3dc5ff52
rovallec <raul.ovalle@nearsol.gt>
2020-12-03 21:49:40 -0600

    a

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -11,19 +11,19 @@ import {Observable} from 'rxjs';
 })
 export class ApiService {
 
-  php_api_server = "http://localhost/angularproject"; // desarrollo  
-//PHP_API_SERVER = "http://200.94.251.67";  // produccion
+//PHP_API_SERVER = "http://localhost/angularproject"; // desarrollo  
+PHP_API_SERVER = "http://200.94.251.67";  // produccion
 
   createProfile(profile:profiles){
-    return this.httpClient.post<number>(`${this.php_api_server}/phpscripts/create_application.php`, profile);
+    return this.httpClient.post<number>(`${this.PHP_API_SERVER}/phpscripts/create_application.php`, profile);
   };
 
   create_job_history(profile:job_histories[]){
-    return this.httpClient.post<number>(`${this.php_api_server}/phpscripts/create_job_history.php`, profile);
+    return this.httpClient.post<number>(`${this.PHP_API_SERVER}/phpscripts/create_job_history.php`, profile);
   }
 
   create_family_information(profile:Family_Information[]){
-    return this.httpClient.post<number>(`${this.php_api_server}/phpscripts/create_family_history.php`, profile);
+    return this.httpClient.post<number>(`${this.PHP_API_SERVER}/phpscripts/create_family_history.php`, profile);
   }
 
   constructor(private httpClient: HttpClient) {
